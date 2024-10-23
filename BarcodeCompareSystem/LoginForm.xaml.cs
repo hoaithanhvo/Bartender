fe64cc2ab1e2b701e8a4ddc06304ed96b4c8e85b
hoaithanhvo <115620157+hoaithanhvo@users.noreply.github.com>
2024-10-24 00:39:09 +0700

    mold commit 2

--- BarcodeCompareSystem/LoginForm.xaml.cs
+++ BarcodeCompareSystem/LoginForm.xaml.cs
@@ -41,7 +41,7 @@ namespace BarcodeCompareSystem
         private void btnCancel_Click(object sender, RoutedEventArgs e) {
             this.DialogResult = false;
             this.Close();
-            //mold commit 1
+            //mold commit 2
             this.CenterWindowOnScreen();
         }
     }
