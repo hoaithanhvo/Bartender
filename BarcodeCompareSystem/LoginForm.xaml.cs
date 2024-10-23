59d967ad02df687f325ff5d8499bd98927c382ef
hoaithanhvo <115620157+hoaithanhvo@users.noreply.github.com>
2024-10-23 23:07:13 +0700

    smt commit3

--- BarcodeCompareSystem/LoginForm.xaml.cs
+++ BarcodeCompareSystem/LoginForm.xaml.cs
@@ -41,6 +41,7 @@ namespace BarcodeCompareSystem
         private void btnCancel_Click(object sender, RoutedEventArgs e) {
             this.DialogResult = false;
             this.Close();
+            //smt commit3 
         }
     }
 }
